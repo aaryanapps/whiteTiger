4b28498168a477c64e44bb23f3aba8f4e60caa72
Saumil Mehta <smehta@aaryanapps.com>
2008-08-20 23:27:05 +0000

    Adding Bll.core.Test

---
+++ BLL.core.Test/include/StdAfx.h
@@ -0,0 +1,25 @@
+
+#ifndef _WT_STDAFX_H
+#define _WT_STDAFX_H
+/*
+#include <list>
+#include <vector>
+#include <map>
+#include <string>
+#include <sstream>
+*/
+
+#ifndef _WIN32_WINNT
+    #define _WIN32_WINNT 0x400
+#endif
+
+#ifdef WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <winsock2.h>
+#include <windows.h>
+#undef GetObject
+#endif
+
+
+
+#endif
---
+++ BLL.core.Test/src/coreTest.cpp
@@ -0,0 +1,13 @@
+#include "StdAfx.h"
+
+#include <iostream>
+
+
+int main(int argc, const char* argv[])
+{
+
+	std::cout << "Core.Test compiled and run successfully!!" << std::endl;
+
+	return 0;
+
+}
